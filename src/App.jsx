019a83b9c2da61e39129553de1f5c74a13ed9115
sakibHasan7439 <sakibhasanb120@gmail.com>
2024-10-22 06:22:28 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import BookMarks from './components/BookMarks/BookMarks'
+import Header from './components/Header/Header'
+
+function App() {
+
+  return (
+
+    <>
+      <Header/>
+      <div className='md:flex'>
+          <Blogs/>
+          <BookMarks/>
+      </div>
+    </>
+  )
+}
+
+export default App
